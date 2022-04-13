60501155cb306c0acc50770c827c36dcf6d5f8e8
vicent.mateu <vicent.mateu@onlean.es>
2022-04-13 11:40:46 +0200

    taskList View + delete + info + modify Buttons

--- src/main/java/com/HandyPerson/servlet/GetAssigmentsServlet.java
+++ src/main/java/com/HandyPerson/servlet/GetAssigmentsServlet.java
@@ -27,14 +27,21 @@ public class GetAssigmentsServlet extends HttpServlet {
         out.println("<head>\n" +
                 "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n" +
                 "</head>");
-        out.println("<div class='container'>");
-        out.println("<h1>Listado de libros</h1>");
+        out.println("<div class='container mt-5 w-25'>");
+        out.println("<h5 class=\"text-secondary text-center\">List of Assignments In your Area</h5>");
         Database database = new Database();
         TaskDao taskDao = new TaskDao(database.getConnection());
-        out.println("<ul class='list-group'>");
+        out.println("<ul class='list-group list-group-flush'>");
         List<Task> tasks = taskDao.findAll();
         for (Task task : tasks) {
-            out.println("<li class='list-group-item'><a href='book.jsp?id=" + task.getId() + "'>" + task.getTitle() + "</a></li>");
+            out.println("<li style=\"background-color: #e9ecef;;\" class=\"d-flex align-items-start d-flex list-group-item\">");
+            out.println("<a class=\"text-secondary text-decoration-none\" href='book.jsp?id=" + task.getId() + "'>" + task.getTitle() + "</a>");
+            out.println("</li>");
+            out.println("<li class=\"d-flex justify-content-between mb-5 mt-1 \">");
+            out.println("<button type=\"button\" class=\"  btn btn-danger\">DELETE</button>");
+            out.println("<button type=\"button\" class=\"btn btn-info\">+Info</button>");
+            out.println("<button type=\"button\" class=\"  btn btn-secondary\">MODIFY</button>");
+            out.println("</li>");
         }
         out.println("</ul>");
         out.println("</div>");
